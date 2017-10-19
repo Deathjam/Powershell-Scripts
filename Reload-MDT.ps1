4e767105c121a75756f5b559f03cf375ae21a602
Phil <philwray@gmail.com>
2017-10-19 09:02:19 +0100

    Update Reload-MDT.ps1

--- Reload-MDT.ps1
+++ Reload-MDT.ps1
@@ -1,12 +1,13 @@
 Import-Module "C:\Program Files\Microsoft Deployment Toolkit\bin\MicrosoftDeploymentToolkit.psd1"
-#new-PSDrive -Name "DS002" -PSProvider "MDTProvider" -Root "E:\Shares\DeploymentShare" -Description "MDT Deployment Share" -NetworkPath "\\SERVER\DeploymentShare$" -Verbose | add-MDTPersistentDrive -Verbose
+#mount deployment share as a psdrive
 New-PSDrive -Name "DS002" -PSProvider MDTProvider -Root "E:\Shares\DeploymentShare"
+#Completely regenerate the boot images
 Update-MDTDeploymentShare -Path "DS002:" -Verbose
-
+#replace boot images
 $Servers = "WDSServer1", "WDSServer2", "WDSServer3"
 Foreach($Server in $Servers){
     wdsutil.exe /Verbose /Progress /Replace-Image /Image:"Lite Touch Windows PE (x86)" /Server:$Server /ImageType:Boot /Architecture:x86 /ReplacementImage /ImageFile:"E:\Shares\DeploymentShare\Boot\LiteTouchPE_x86.wim"
     wdsutil.exe /Verbose /Progress /Replace-Image /Image:"Lite Touch Windows PE (x64)" /Server:$Server /ImageType:Boot /Architecture:x64 /ReplacementImage /ImageFile:"E:\Shares\DeploymentShare\Boot\LiteTouchPE_x64.wim"
 }
-
+#Restart WDS service
 Get-Service -Name WDSServer | Restart-Service -Verbose
