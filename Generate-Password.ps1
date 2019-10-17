dc66864ed8e864b683df6e96624047d2ad1f2938
Phil <philwray@gmail.com>
2019-10-17 16:52:26 +0100

    added choice for symbol

--- Generate-Password.ps1
+++ Generate-Password.ps1
@@ -3,32 +3,67 @@ Function Generate-Password
   param
   (
     [Parameter(Mandatory = $true)]
-    [Int]$Count
-  )   
-  $PasswordCount = 0
-  Do 
-  {  
-    $path = 'c:\temp\fourlist.txt'
-    if (!( Test-Path $path))
-    {
-      New-Item -ItemType Directory -Path C:\temp\
-      Invoke-WebRequest -Uri 'https://s3-eu-west-1.amazonaws.com/public-fourlist/fourlist.txt' -OutFile $path 
-      Start-Sleep -Seconds 2
-    }
-    $four = Get-Content -Path $path
-    $symbolarr = @("$", '^', '&', '£', '%')
-    $sym = Get-Random -Maximum $symbolarr
-    $first = Get-Random -Maximum  $four
-    $second = Get-Random -Maximum $four
-    $last = Get-Random -Minimum 100 -Maximum 999 
-    $Password = $sym+$first+$second+$last
-    if($four.Length -gt 3)
-    {
-      [pscustomobject]@{
-        Password = -join $Password
+    [Int]$Count,
+    [Parameter(Mandatory = $false)]
+    [Switch]$symbol 
+  )
+  if(!($symbol)){
+    $PasswordCount = 0
+    Do 
+    {  
+      $path = 'c:\temp\fourlist.txt'
+      if (!( Test-Path $path))
+      {
+        New-Item -ItemType Directory -Path C:\temp\
+        Invoke-WebRequest -Uri 'https://s3-eu-west-1.amazonaws.com/public-fourlist/fourlist.txt' -OutFile $path 
+        Start-Sleep -Seconds 2
       }
+      $four = Get-Content -Path $path
+      #$symbolarr = @("$", '^', '&', '£', '%')
+      #$sym = Get-Random -Maximum $symbolarr
+      $first = Get-Random -Maximum  $four
+      $second = Get-Random -Maximum $four
+      $last = Get-Random -Minimum 100 -Maximum 999 
+      $Password = $first+$second+$last
+      if($four.Length -gt 3)
+      {
+        [pscustomobject]@{
+          Password = -join $Password
+        }
+      }
+      $PasswordCount++
     }
-    $PasswordCount++
+    until ($PasswordCount -eq $Count)
   }
-  until ($PasswordCount -eq $Count)
+  else {
+    $PasswordCount = 0
+    Do 
+    {  
+      $path = 'c:\temp\fourlist.txt'
+      if (!( Test-Path $path))
+      {
+        New-Item -ItemType Directory -Path C:\temp\
+        Invoke-WebRequest -Uri 'https://s3-eu-west-1.amazonaws.com/public-fourlist/fourlist.txt' -OutFile $path 
+        Start-Sleep -Seconds 2
+      }
+      $four = Get-Content -Path $path
+      $symbolarr = @("$", '^', '&', '£', '%')
+      $sym = Get-Random -Maximum $symbolarr
+      $first = Get-Random -Maximum  $four
+      $second = Get-Random -Maximum $four
+      $last = Get-Random -Minimum 100 -Maximum 999 
+      $Password = $sym+$first+$second+$last
+      if($four.Length -gt 3)
+      {
+        [pscustomobject]@{
+          Password = -join $Password
+        }
+      }
+      $PasswordCount++
+    }
+    until ($PasswordCount -eq $Count)
+  }  
 }
+
+
+Generate-Password -Count 5 -symbol:$true
